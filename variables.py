d1f61035fc6e30b28369a821588bff5014456b99
Anisa <anisahamohamed@gmail.com>
2020-05-24 18:49:52 -0400

    python variable refresher

---
+++ variables.py
@@ -0,0 +1,16 @@
+import turtle
+a = 5
+print(a)
+b = 'Hi Anisa!'
+print(b)
+credit_card = 488347722
+print(credit_card)
+anisa_turtle = turtle.Turtle()
+# create a square with a turtle
+anisa_turtle.forward(100)
+anisa_turtle.right(90)
+anisa_turtle.forward(100)
+anisa_turtle.right(90)
+anisa_turtle.forward(100)
+anisa_turtle.right(90)
+anisa_turtle.forward(100)
