6d943c90e2136a5cdfd84bfaf7a62a3ed7a92b8a
Anisa <anisahamohamed@gmail.com>
2020-05-25 12:36:27 -0400

    for list  python refresher

---
+++ list.py
@@ -0,0 +1,19 @@
+l = [1, 2, 'a', [3, 4], True, 5.5]
+print(l)
+l2 = [1, 200, 39, 47, 5, 17]
+print(l2[0])
+names = ['joe', 'john', 'james']
+print(names)
+names.append('anisa')
+print(names)
+names.insert(0, 'mohamed')
+print(names)
+names.remove('john')
+print(names)
+names.reverse()
+print(names)
+print(l2)
+l2.sort()
+print(l2)
+for number in l2:
+    print(number)
